8a252d00880cb892944933ef8d0217feb5c7c379
waleadekoya <50258092+waleadekoya@users.noreply.github.com>
2022-12-09 06:24:04 +0000

    Update items_loader.py

--- src/utils/items_loader.py
+++ src/utils/items_loader.py
@@ -8,15 +8,18 @@ from config import recipients, title
 
 reed_jobs = Path(__file__).parent.parent / "maredo_web_crawler/reed_jobs.json"
 print(reed_jobs)
-data = pd.read_json(reed_jobs)
+try:
+    data = pd.read_json(reed_jobs)
 
-print(data)
+    print(data)
 
-SendMultipartEmail(
-    subject=f'{title} Reed Jobs',
-    sender='wale.adekoya@btinternet.com',
-    recipients=recipients,
-    attachment_body=data.to_html(index=False, col_space='100px')
-)
+    SendMultipartEmail(
+        subject=f'{title} Reed Jobs',
+        sender='wale.adekoya@btinternet.com',
+        recipients=recipients,
+        attachment_body=data.to_html(index=False, col_space='100px')
+    )
 
-reed_jobs.unlink()
+    reed_jobs.unlink()
+except ValueError as e:
+    print(e)
